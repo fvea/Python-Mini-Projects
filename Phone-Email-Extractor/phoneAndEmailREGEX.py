79a074c54caf55379dac0f63fd68c68446b43425
fvea <fjvinceatabay@gmail.com>
2021-02-02 21:39:51 +0800

    PhoneNum and Email Extractor using REGEX DONE

--- Phone-Email-Extractor/phoneAndEmailREGEX.py
+++ Phone-Email-Extractor/phoneAndEmailREGEX.py
@@ -15,7 +15,7 @@ import re, pyperclip
 phoneREGEX = re.compile(r'''(
     (\d{3}|\(\d{3}\))?                  # area code
     (\s|-|\.)?                          # seperator
-    \d{3}                               # first 3 digits
+    (\d{3})                             # first 3 digits
     (\s|-|\.)                           # seperator
     (\d{4})                             # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension
@@ -29,4 +29,23 @@ emailREGEX = re.compile(r'''(
     (\.[a-zA-Z]{2,4})                   # dot-something
 )''', re.VERBOSE)
 
+# Find matches in the clipboard text.
+text = str(pyperclip.paste())
 
+matches = []
+for groups in phoneREGEX.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailREGEX.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    matchesFormatted = '\n'.join(matches)
+    pyperclip.copy(matchesFormatted)
+    print('Copied to the clipboard:')
+    print(matchesFormatted)
+else:
+    print('No phone numbers or email addresses found.')
