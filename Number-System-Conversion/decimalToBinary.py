149660e6f3b50305acb74c92699166112772e737
fvea <fjvinceatabay@gmail.com>
2021-02-04 21:14:44 +0800

    Initial Commit: Project Started

---
+++ Number-System-Conversion/decimalToBinary.py
@@ -0,0 +1,17 @@
+#! python3
+# decimalToBinary.py
+
+def decToBi(dec):
+    ''' 
+    Returns the Binary Representation of the given decimal. 
+    '''
+    if dec == 1:
+        return '1'
+    else:
+        q = dec // 2
+        r = str(dec % 2)
+        return decToBi(q) + r
+
+if __name__ == '__main__':
+    ex = 34
+    print(decToBi(ex))
