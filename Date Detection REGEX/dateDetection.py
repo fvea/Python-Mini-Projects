bcfdbb69f0ef15487a9d51f12ee959e121503047
fvea <fjvinceatabay@gmail.com>
2021-02-04 18:30:07 +0800

    dateREGEX ADDED

--- Date Detection REGEX/dateDetection.py
+++ Date Detection REGEX/dateDetection.py
@@ -2,3 +2,29 @@
 # dateDetection.py 
 #   - detects dates in DD/MM/YYYY format in a given text.
 
+# TODO: DATE REGEX
+#   Create regular expression for the dates in DD/MM/YYYY format.
+#   Assume that days, months and years range from 01-31, 01-12, and
+#   1000-2999 respectively.
+
+import re
+
+dateREGEX = re.compile(r'''(
+    ((0[1-9])|([1-2]\d)|(3[0-1]))   # day 01-31
+    (/|-)                           # seperator
+    ((0[1-9])|(1[0-2]))             # month 01-12 
+    (/|-)                           # seperator
+    ([1-2]\d{3})                    # year 1000-2999 
+)''', re.VERBOSE)
+
+
+
+
+# TODO:
+#   Fetch month, day and year from detected date.
+#   Check if the date is valid, if yes add it to the 
+#   list of detected dates. 
+
+# TODO:
+#   Print the dates found, or print a message if no dates are found.
+
