31ac649420814d95a5ee0af760f19a913900287a
fvea <fjvinceatabay@gmail.com>
2021-02-04 19:32:47 +0800

    Strong Password Detection Using Regex STARTED

---
+++ Strong-Password-Detection-REGEX/strongPasswd.py
@@ -0,0 +1,14 @@
+#! python3
+# strongPasswd.py
+#   - contains isStrong(passwd) function that checks wether the
+#     given passwd is strong.
+#
+# Criteria used for a strong password.
+# 1. Atleast 8 chars long.
+# 2. Contains both uppercase and lowercase letters.
+# 3. Contains atleast 1 digit.
+
+import re
+
+def isStrong(passwd):
+    pass
