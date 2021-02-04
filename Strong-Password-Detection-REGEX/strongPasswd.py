a907f36ed8b841c10aff9c8aea6b2631614c901a
fvea <fjvinceatabay@gmail.com>
2021-02-04 19:54:15 +0800

    Strong Password Detection Using REGEX DONE

--- Strong-Password-Detection-REGEX/strongPasswd.py
+++ Strong-Password-Detection-REGEX/strongPasswd.py
@@ -11,4 +11,22 @@
 import re
 
 def isStrong(passwd):
-    pass
+    ''' Returns True if the password is strong. '''
+    
+    # REGEX patterns that the given password must followed.
+    digitsREGEX = re.compile(r'\d+')
+    lowercaseREGEX = re.compile(r'[a-z]+')
+    uppercaseREGEX = re.compile(r'[A-Z]+')
+    lenREGEX = re.compile(r'.{8,}')
+
+    if lenREGEX.search(passwd) and lowercaseREGEX.search(passwd) and \
+        uppercaseREGEX.search(passwd) and digitsREGEX.search(passwd):
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    passwordOne = '9eFS463B'
+    print(isStrong(passwordOne)) # prints true
+    passwordTwo = 'fveataba3'
+    print(isStrong(passwordTwo)) # prints false
