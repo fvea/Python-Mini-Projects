967de4549d673899b2ad9049c0f6d55566b6a906
fvea <fjvinceatabay@gmail.com>
2021-02-13 20:33:59 +0800

    Pseudocode WRITTEN

---
+++ Mad-Libs/madLibs.py
@@ -0,0 +1,7 @@
+#! python3
+# madLibs.py - Reads text files and lets the user add their own
+# text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears
+# in the text file.
+
+import re
+from pathlib import Path
