4e421724fce54740e46824a2fca659c230c07bed
fvea <fjvinceatabay@gmail.com>
2021-02-13 21:10:26 +0800

    Mad Libs testing DONE

--- Mad-Libs/madLibs.py
+++ Mad-Libs/madLibs.py
@@ -24,17 +24,37 @@ REGEX = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
 # Read the content of the text file and 
 # find all the occurrence of the Regex pattern.
 with open(filePath / 'sample.txt', 'r') as fileObj:
-    matches = REGEX.findall(fileObj.read())
+    content = fileObj.read()
+    matches = REGEX.findall(content)
 
 if len(matches) == 0:
+    print('No matches(ADJECTIVE, NOUN, ADVERB or VERB) found.')
     sys.exit()
-
 
 # Prompt the user for replacing words.
 replacingWords = []
 for found in matches:
-    print('Enter an %s' % (found.lower()))
+    print('Enter an %s:' % (found.lower()))
     replacingWords.append(input())
+
+# Replace the found patterns by new ones.
+newContent = content
+for word in replacingWords:
+    newContent = REGEX.sub(word, newContent, count=1)
+
+print('\nOld Content:\n' + content + '\n')
+print('New Content:\n' + newContent + '\n')
+
+# Write the modified content to a new file.
+with open(filePath / 'sample_replaced.txt', 'w') as fileObj:
+    fileObj.write(newContent)
+
+print('New content written to: sample_replaced.txt')
+
+
+
+
+
 
 
 
