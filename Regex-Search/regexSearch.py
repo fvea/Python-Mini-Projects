71be5e8234377b4041bd56a1ccd283321977b408
fvea <fjvinceatabay@gmail.com>
2021-02-14 08:48:26 +0800

    RegeX-Search Test Cases DONE

--- Regex-Search/regexSearch.py
+++ Regex-Search/regexSearch.py
@@ -11,3 +11,33 @@
 #   6. Loop through all the files and look for the regex matches.
 #   7. Save the matches in dictionary with keys as the filename and values as the matches.
 #   8. Print the results.
+
+import re
+import pprint
+from pathlib import Path
+
+# Prompt the user for regular expression, and compile it.
+print('Enter a valid regular expression:')
+REGEX = re.compile(r'%s' % input())
+
+# Create a WindowsPath object for the target directory of .txt files.
+targetDir = Path.cwd() / 'Regex-Search'
+
+# Create a list of text files from the target directory.
+textFiles = targetDir.glob('*.txt')
+
+# Look for patterns in the text files.
+results = {}
+for textFile in textFiles:
+    with open(textFile, 'r') as fileObj:
+        matches = REGEX.findall(fileObj.read())
+        if len(matches) > 0:
+            results[textFile.name] = matches
+
+# Print results
+pprint.pprint(results)
+
+
+
+
+
