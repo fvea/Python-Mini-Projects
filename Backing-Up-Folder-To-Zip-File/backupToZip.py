508098ee8e87d9b322ba5cec6ca044d5b8e70327
fvea <fjvinceatabay@gmail.com>
2021-02-18 20:29:13 +0800

    Creating the ZIP File and Archiving DONE

--- Backing-Up-Folder-To-Zip-File/backupToZip.py
+++ Backing-Up-Folder-To-Zip-File/backupToZip.py
@@ -17,3 +17,23 @@ def backupToZip(folder):
         if not os.path.exists(zipFileName):
             break
         number = number + 1
+    
+    # Create the ZIP File.
+    print(f'Creating {zipFileName}...')
+    backupZip = zipfile.ZipFile(zipFileName, 'w')
+
+    # Walk the entire folder tree and compress the files in each folder.
+    for foldername, subfolders, filenames, in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        # Add the current folder to the ZIP File.
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue    # don't backup the backup ZIP files.
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Done.')
+
