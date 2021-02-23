f6eff90715a26988c127579cb532482859e2a93e
fvea <fjvinceatabay@gmail.com>
2021-02-23 19:40:25 +0800

    Selective Copy Project Started - Logic Written

---
+++ Selective-Copy/selectiveCopy.py
@@ -0,0 +1,17 @@
+#! python3
+# selectiveCopy.py - walks through a folder tree and searches for files with
+# a certain file extension(.txt) and copies these files from whatever location
+# they are into a new folder.
+
+
+# LOGIC:
+#   1. import shutil for copying folder mechanism.
+#   2. import os for path handling.
+#   3. create function selectiveCopy()
+#   4. walk to the given path folder.
+#   5. if the current filename in current folder ends with .txt extension, copy it to destination folder.
+
+
+
+def selectiveCopy(path, destination):
+    pass
