1e6b5299d828e57a0143cff147de82db9c2005aa
fvea <fjvinceatabay@gmail.com>
2021-02-24 13:15:10 +0800

    unneededFiles.py LOGIC Written

--- Unneeded-Files/unneededFiles.py
+++ Unneeded-Files/unneededFiles.py
@@ -3,3 +3,8 @@
 # and searches for exceptionally large files or folders—say, ones 
 # that have a file size of more than 100MB.
 
+# Logic:
+#   1. create function named unneededFiles(path).
+#   2. walk through the given path.
+#   3. check if current file in list of filenames in the current
+#       folder has a size greater than 100MB, if yes, print it.
