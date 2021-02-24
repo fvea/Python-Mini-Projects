2bdbb00ac94ce14c97f850d508b07c3b6754ac70
fvea <fjvinceatabay@gmail.com>
2021-02-24 13:32:03 +0800

    Function DONE and Test Cases ADDED

--- Unneeded-Files/unneededFiles.py
+++ Unneeded-Files/unneededFiles.py
@@ -8,3 +8,24 @@
 #   2. walk through the given path.
 #   3. check if current file in list of filenames in the current
 #       folder has a size greater than 100MB, if yes, print it.
+
+import os
+
+def unneededFiles(path):
+    """
+    Walks the folder in the given path, and prints
+    all the files with the file size greater than 100MB.
+    """
+    path = os.path.abspath(path)    # Make sure the path is absolute.
+
+    for folderName, subfolders, fileNames in os.walk(path):
+        for filename in fileNames:
+            filepath = os.path.join(path, folderName, filename)
+            fileSize = os.path.getsize(filepath)
+            if fileSize > 100e6:
+                print(f'{filepath} has size {fileSize}bytes.')
+    print('DONE.')
+
+if __name__ == '__main__':
+    samplePath = r'C:\Users\fvea\Downloads'
+    unneededFiles(samplePath)
