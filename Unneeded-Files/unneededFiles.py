026c5ae8a6789342b8c43d410b4184592b77fcdc
fvea <fjvinceatabay@gmail.com>
2021-02-24 13:12:56 +0800

    unneededFiles.py STARTED and documentation WRITTEN

---
+++ Unneeded-Files/unneededFiles.py
@@ -0,0 +1,5 @@
+#!python3
+# unneededFiles.py - program that walks through a folder tree 
+# and searches for exceptionally large files or folders—say, ones 
+# that have a file size of more than 100MB.
+
