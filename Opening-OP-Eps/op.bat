32b3a69dd912cce1b1ab23a7f2e3d9d962774f1e
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-03-07 15:31:43 +0800

    Batch Files and Shelf Files ADDED

---
+++ Opening-OP-Eps/op.bat
@@ -0,0 +1,2 @@
+@python C:\Users\fjvin\Python-Mini-Projects\Opening-OP-Eps\op.py %*
+@pause
--- Opening-OP-Eps/op.py
+++ Opening-OP-Eps/op.py
@@ -5,14 +5,15 @@
 import os, sys, shelve, webbrowser
 
 URL = 'https://www13.9anime.to/watch/one-piece.ov8/ep-'
-shelfPath = os.path.join(os.getcwd, 'Opening-OP-Eps')
+shelfPath = os.path.join(os.getcwd(), r'Python-Mini-Projects\Opening-OP-Eps')
 shelfFileName = 'currentEps'
 
 def updateShelfFile(eps):
+    eps = str(int(eps) + 1)
     with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
         db['currentEps'] = eps
 
-def getEps(eps):
+def getEps():
     with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
         currentEps = db['currentEps']
     return currentEps
@@ -20,9 +21,10 @@ def getEps(eps):
 def main():
     if len(sys.argv) > 1:
         eps = ' '.join(sys.argv[1:])
+        del sys.argv[1:]
     else:
         eps = getEps()
-    updateShelfFile(str(int(eps) + 1))
+    updateShelfFile(eps)
     
     nextEpsURL = URL + eps
     print('Opening {}'.format(nextEpsURL))
