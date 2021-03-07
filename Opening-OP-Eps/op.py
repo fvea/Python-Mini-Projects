a800c6a97a88ee8715fa89010ff2d7e0ac1e1940
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-03-07 15:04:12 +0800

    Codebase UPDATED

--- Opening-OP-Eps/op.py
+++ Opening-OP-Eps/op.py
@@ -2,4 +2,39 @@
 # op.py
 #   - Opens the web browser to 9anime.to for watching anime One Piece.
 #   - Tracks the current episode or can feed cmd arguments for the episode.
+import os, sys, shelve, webbrowser
+
+URL = 'https://www13.9anime.to/watch/one-piece.ov8/ep-'
+shelfPath = os.path.join(os.getcwd, 'Opening-OP-Eps')
+shelfFileName = 'currentEps'
+
+def updateShelfFile(eps):
+    with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
+        db['currentEps'] = eps
+
+def getEps(eps):
+    with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
+        currentEps = db['currentEps']
+    return currentEps
+
+def main():
+    if len(sys.argv) > 1:
+        eps = ' '.join(sys.argv[1:])
+    else:
+        eps = getEps()
+    updateShelfFile(str(int(eps) + 1))
+    
+    nextEpsURL = URL + eps
+    print('Opening {}'.format(nextEpsURL))
+    webbrowser.open(nextEpsURL)
+
+if __name__ == '__main__':
+    ans = ''
+    while(ans != 'n'):
+        main()
+        ans = input('Jump to next eps?[y] Yes [n] No: ')
+
+
+
+
 
