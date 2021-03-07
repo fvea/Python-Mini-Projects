49647f051a9b0f3c1226f1ea95e404b85cae6bb8
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-03-07 16:33:00 +0800

    Documentation ADDED

--- Opening-OP-Eps/op.py
+++ Opening-OP-Eps/op.py
@@ -9,32 +9,37 @@ shelfPath = os.path.join(os.getcwd(), r'Python-Mini-Projects\Opening-OP-Eps')
 shelfFileName = 'currentEps'
 
 def updateShelfFile(eps):
+    '''
+        Updates the shelf file with the next episode.
+    '''
     eps = str(int(eps) + 1)
     with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
         db['currentEps'] = eps
 
 def getEps():
+    '''
+        Returns the current episode stored on the shelf file.
+    '''
     with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
         currentEps = db['currentEps']
     return currentEps
 
 def main():
     if len(sys.argv) > 1:
-        eps = ' '.join(sys.argv[1:])
+        eps = sys.argv[1]
         del sys.argv[1:]
     else:
         eps = getEps()
+
+    epsURL = URL + eps
+    print('Opening {}...'.format(epsURL))
+    webbrowser.open(epsURL)
+    print("You're currently watching episode-{}...".format(eps))
     updateShelfFile(eps)
-    
-    nextEpsURL = URL + eps
-    print('Opening {}'.format(nextEpsURL))
-    webbrowser.open(nextEpsURL)
+    print('Next episode updated to: {}'.format(getEps()))
 
 if __name__ == '__main__':
-    ans = ''
-    while(ans != 'n'):
-        main()
-        ans = input('Jump to next eps?[y] Yes [n] No: ')
+    main()
 
 
 
