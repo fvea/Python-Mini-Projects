11358899ce7776995637f9f0be6f54a001a3a45d
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-03-15 21:46:21 +0800

    Code Refactored

--- Opening-OP-Eps/op2.py
+++ Opening-OP-Eps/op2.py
@@ -5,18 +5,23 @@
 #   - Tracks the current episode or can feed cmd arguments for the episode.
 from selenium import webdriver
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
-import pyinputplus as pyip
 import sys, shelve, os, re
 
 class MyListener(AbstractEventListener):
+    '''
+        Edge Webdriver with overriden before_quit() method.
+    '''
     def before_quit(self, driver):
+        '''
+            Updates the shelf file with the next episode.
+        '''
         updateNextEps(epsRE.search(driver.current_url).group())
-        print('Next episode updated to {nextEps}'.format(nextEps=getEps()))
+        print('Next episode updated to {nextEps}.'.format(nextEps=getEps()))
 
 def main(eps):
-    edgeDriver = EventFiringWebDriver(MyEdge(), MyListener())
+    edgeDriver = EventFiringWebDriver(webdriver.Edge(), MyListener())      # Create an instance of the Edge Webdriver.
     edgeDriver.get('https://www12.9anime.to/watch/one-piece.ov8/ep-' + eps)
-    pyip.inputYesNo(prompt='Stop Watching? [Y] Yes [N] No')
+    input()
     edgeDriver.quit()
 
 def updateNextEps(eps):
@@ -38,7 +43,7 @@ if __name__ == '__main__':
     epsRE = re.compile(r'\d+$')                                             # Pattern for extracting episode number at end of an URL.
     shelfFile = os.path.join(                                               # Shelf file path.
         os.getcwd(),
-        r'Python-Mini-Projects\Opening-OP-Eps\currentEps.dat'
+        r'Python-Mini-Projects\Opening-OP-Eps\currentEps'
     )
     if len(sys.argv) > 1:
         main(eps=sys.argv[1])
