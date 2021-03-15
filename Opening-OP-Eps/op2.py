b4f201968d6f1d80750963ed0c809c6d1470e297
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-03-15 21:31:31 +0800

    Project DONE

--- Opening-OP-Eps/op2.py
+++ Opening-OP-Eps/op2.py
@@ -8,38 +8,38 @@ from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEven
 import pyinputplus as pyip
 import sys, shelve, os, re
 
-epsRE = re.compile(r'\d+$')
-URL = 'https://www12.9anime.to/watch/one-piece.ov8/ep-'
-shelfPath = os.path.join(os.getcwd(), r'Python-Mini-Projects\Opening-OP-Eps')
-shelfFileName = 'currentEps'
-
 class MyListener(AbstractEventListener):
     def before_quit(self, driver):
         updateNextEps(epsRE.search(driver.current_url).group())
         print('Next episode updated to {nextEps}'.format(nextEps=getEps()))
 
+def main(eps):
+    edgeDriver = EventFiringWebDriver(MyEdge(), MyListener())
+    edgeDriver.get('https://www12.9anime.to/watch/one-piece.ov8/ep-' + eps)
+    pyip.inputYesNo(prompt='Stop Watching? [Y] Yes [N] No')
+    edgeDriver.quit()
+
 def updateNextEps(eps):
     '''
         Updates the shelf file with the next episode.
     '''
-    with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
+    with shelve.open(shelfFile) as db:
         db['currentEps'] = str(int(eps) + 1)
 
 def getEps():
     '''
         Returns the current episode stored on the shelf file.
     '''
-    with shelve.open(os.path.join(shelfPath, shelfFileName)) as db:
+    with shelve.open(shelfFile) as db:
         currentEps = db['currentEps']
     return currentEps
-
-def main(eps):
-    edgeDriver = EventFiringWebDriver(MyEdge(), MyListener())
-    edgeDriver.get(URL + eps)
-    pyip.inputYesNo(prompt='Stop Watching? [Y] Yes [N] No')
-    edgeDriver.quit()
     
 if __name__ == '__main__':
+    epsRE = re.compile(r'\d+$')                                             # Pattern for extracting episode number at end of an URL.
+    shelfFile = os.path.join(                                               # Shelf file path.
+        os.getcwd(),
+        r'Python-Mini-Projects\Opening-OP-Eps\currentEps.dat'
+    )
     if len(sys.argv) > 1:
         main(eps=sys.argv[1])
     else:
