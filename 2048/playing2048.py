097a53caa3224ed29e3bc6e3ddb5bc0aafef40ab
Fj Vincent Atabay <fjvinceatabay@gmail.com>
2021-07-19 12:21:36 +0800

    Documentation added for 2048 game playing AI

--- 2048/playing2048.py
+++ 2048/playing2048.py
@@ -1,3 +1,11 @@
+"""
+2048 is a simple game where you combine tiles by sliding them up, down, 
+left, or right with the arrow keys. You can actually get a fairly high score 
+by repeatedly sliding in an up, right, down, and left pattern over and over 
+again. This program will open the game at https://gabrielecirulli.github.io/2048/ 
+and keep sending up, right, down, and left keystrokes to automatically play the game.
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
